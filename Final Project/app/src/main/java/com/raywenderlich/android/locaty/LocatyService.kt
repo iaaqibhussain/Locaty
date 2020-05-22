5585ebb284800a423b88a17128bb6ebf38b0be20
Aaqib Hussain <aaqib.hussain@olx.com>
2020-05-22 17:29:45 +0200

    added starter and final project.

---
+++ Final Project/app/src/main/java/com/raywenderlich/android/locaty/LocatyService.kt
@@ -0,0 +1,178 @@
+/**
+ * Copyright (c) 2020 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.raywenderlich.android.locaty
+
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.Service
+import android.content.Context
+import android.content.Intent
+import android.hardware.Sensor
+import android.hardware.SensorEvent
+import android.hardware.SensorEventListener
+import android.hardware.SensorManager
+import android.os.Build
+import android.os.IBinder
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.NotificationCompat
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
+import kotlin.math.round
+
+
+class LocatyService : Service(), SensorEventListener {
+
+    lateinit var sensorManager: SensorManager
+    private val accelerometerReading = FloatArray(3)
+    private val magnetometerReading = FloatArray(3)
+    private val rotationMatrix = FloatArray(9)
+    private val orientationAngles = FloatArray(3)
+    var background = false
+    private val NOTIFICATION_ID = 1
+
+    companion object {
+        val KEY_ANGLE = "angle"
+        val KEY_DIRECTION = "direction"
+        val KEY_BACKGROUND = "background"
+        val KEY_ON_SENSOR_CHANGED_ACTION = "com.raywenderlich.android.locaty.ON_SENSOR_CHANGED"
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+        sensorManager = getSystemService(AppCompatActivity.SENSOR_SERVICE) as SensorManager
+
+        sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER)?.also { accelerometer ->
+            sensorManager.registerListener(this, accelerometer, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI)
+        }
+        sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD)?.also { magneticField ->
+            sensorManager.registerListener(this, magneticField, SensorManager.SENSOR_DELAY_NORMAL, SensorManager.SENSOR_DELAY_UI)
+        }
+    }
+
+    override fun onBind(intent: Intent?): IBinder? {
+        return null
+    }
+
+    override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        intent?.let {
+            background = it.getBooleanExtra(KEY_BACKGROUND, false)
+        }
+
+        return START_STICKY
+    }
+
+    override fun onSensorChanged(event: SensorEvent?) {
+        if (event == null) {
+            return
+        }
+
+        if (event.sensor.type == Sensor.TYPE_ACCELEROMETER) {
+            System.arraycopy(event.values, 0, accelerometerReading, 0, accelerometerReading.size)
+        } else if (event.sensor.type == Sensor.TYPE_MAGNETIC_FIELD) {
+            System.arraycopy(event.values, 0, magnetometerReading, 0, magnetometerReading.size)
+        }
+        updateOrientationAngles()
+    }
+
+    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+    }
+
+    fun updateOrientationAngles() {
+        SensorManager.getRotationMatrix(rotationMatrix, null, accelerometerReading, magnetometerReading)
+
+        val orientation = SensorManager.getOrientation(rotationMatrix, orientationAngles)
+
+        val degrees = (Math.toDegrees(orientation.get(0).toDouble()) + 360.0) % 360.0
+        val direction = getDirection(degrees)
+        val angle = round(degrees * 100) / 100
+
+        val intent = Intent();
+        intent.putExtra(KEY_ANGLE, angle)
+        intent.putExtra(KEY_DIRECTION, direction)
+        intent.action = KEY_ON_SENSOR_CHANGED_ACTION
+
+        LocalBroadcastManager.getInstance(applicationContext).sendBroadcast(intent)
+
+        if (background) {
+            val notification = createNotification(direction, angle)
+            startForeground(NOTIFICATION_ID, notification)
+        } else {
+            stopForeground(true)
+        }
+    }
+
+    private fun createNotification(direction: String, angle: Double): Notification {
+
+        val notificationManager =
+            getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                application.packageName,
+                "Notifications", NotificationManager.IMPORTANCE_DEFAULT
+            )
+
+            // Configure the notification channel.
+            notificationChannel.enableLights(false)
+            notificationChannel.setSound(null, null)
+            notificationChannel.setShowBadge(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
+
+        val notificationBuilder = NotificationCompat.Builder(baseContext, application.packageName)
+        notificationBuilder.setAutoCancel(true)
+            .setDefaults(Notification.DEFAULT_ALL)
+            .setContentTitle(resources.getString(R.string.app_name))
+            .setContentText("You're currently facing ${direction} at an angle of ${angle}°")
+            .setWhen(System.currentTimeMillis())
+            .setSound(null)
+            .setSmallIcon(R.mipmap.ic_launcher)
+
+
+        return notificationBuilder.build()
+    }
+
+    private fun getDirection(angle: Double): String {
+        var direction = ""
+
+        if (angle >= 350 || angle <= 10)
+            direction = "N"
+        if (angle < 350 && angle > 280)
+            direction = "NW"
+        if (angle <= 280 && angle > 260)
+            direction = "W"
+        if (angle <= 260 && angle > 190)
+            direction = "SW"
+        if (angle <= 190 && angle > 170)
+            direction = "S"
+        if (angle <= 170 && angle > 100)
+            direction = "SE"
+        if (angle <= 100 && angle > 80)
+            direction = "E"
+        if (angle <= 80 && angle > 10)
+            direction = "NE"
+
+        return direction
+    }
+}
